00705a65c7a7ef1c7367e0b67f48c6c7155657d7
Prakhar Sharma <prakhar29sharma@gmail.com>
2024-03-29 14:09:21 +0530

    created app info map

---
+++ lib/models/app_info.dart
@@ -0,0 +1,87 @@
+final Map<String, Map<String, String>> appInfoMap = {
+  "com.example.digital_wellbeing": {
+    "name": "Digital Wellbeing",
+    "imagePath": "assets/apps/digital_wellbeing.png",
+  },
+  "com.whatsapp": {
+    "name": "Whatsapp",
+    "imagePath": "assets/apps/whatsapp.png",
+  },
+  "com.instagram.android": {
+    "name": "Instagram",
+    "imagePath": "assets/apps/instagram.png",
+  },
+  "com.spotify.music": {
+    "name": "Spotify",
+    "imagePath": "assets/apps/spotify.png",
+  },
+  "com.snapchat.android": {
+    "name": "Snapchat",
+    "imagePath": "assets/apps/snapchat.png",
+  },
+  "com.linkedin.android": {
+    "name": "LinkedIn",
+    "imagePath": "assets/apps/linkedin.png",
+  },
+  "com.android.settings": {
+    "name": "Settings",
+    "imagePath": "assets/apps/settings.png",
+  },
+  "com.coloros.alarmclock": {
+    "name": "Clock",
+    "imagePath": "assets/apps/clock.png",
+  },
+  "com.coloros.alramclock": {
+    "name": "Clock",
+    "imagePath": "assets/apps/clock.png",
+  },
+  "com.android.chrome": {
+    "name": "Chrome",
+    "imagePath": "assets/apps/chrome.png",
+  },
+  "com.android.vending": {
+    "name": "Play Store",
+    "imagePath": "assets/apps/google_play.png",
+  },
+  "com.google.android.youtube": {
+    "name": "Youtube",
+    "imagePath": "assets/apps/youtube.png",
+  },
+  "in.startv.hotstar": {
+    "name": "Hotstar",
+    "imagePath": "assets/apps/hotstar.png",
+  },
+  "com.zerodha.kite3": {
+    "name": "Kite",
+    "imagePath": "assets/apps/kite.png",
+  },
+  "com.android.providers.calendar": {
+    "name": "Calendar",
+    "imagePath": "assets/apps/calendar.png",
+  },
+  "com.github.android": {
+    "name": "Github",
+    "imagePath": "assets/apps/github.png",
+  },
+  "com.brave.browser": {
+    "name": "Brave",
+    "imagePath": "assets/apps/brave.png",
+  },
+  "com.discord": {
+    "name": "Discord",
+    "imagePath": "assets/apps/discord.png",
+  },
+  "com.truecaller": {
+    "name": "Truecaller",
+    "imagePath": "assets/apps/truecaller.png",
+  },
+  "com.google.android.apps.photos": {
+    "name": "Photos",
+    "imagePath": "assets/apps/photos.png",
+  },
+  "com.twitter.android": {
+    "name": "Twitter",
+    "imagePath": "assets/apps/twitter.png",
+  },
+  // Add more key-value pairs as needed
+};
